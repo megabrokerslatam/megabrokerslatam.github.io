0f944976f8913651cccf3c453940677734a0a46b
Anthony De Rada <anthonyderada@gmail.com>
2018-12-17 00:07:14 -0500

    added github pages support and changed router

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const AppRouter = () => (
   <Router>
